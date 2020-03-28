1eb643d9c52802a8e1a0963812e71cf8ff8eb756
Shayne Burgess <shayne.burgess@gmail.com>
2020-03-27 20:07:41 -0700

    Initial implementation of endpoint suffix property

--- nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/utils/AzureStorageUtils.java
+++ nifi-nar-bundles/nifi-azure-bundle/nifi-azure-processors/src/main/java/org/apache/nifi/processors/azure/storage/utils/AzureStorageUtils.java
@@ -65,7 +65,7 @@ public final class AzureStorageUtils {
             .build();
 
     public static final String ACCOUNT_NAME_BASE_DESCRIPTION =
-            "The storage account name.  There are certain risks in allowing the account name to be stored as a flowfile " +
+            "Shayne The storage account name.  There are certain risks in allowing the account name to be stored as a flowfile " +
             "attribute. While it does provide for a more flexible flow by allowing the account name to " +
             "be fetched dynamically from a flowfile attribute, care must be taken to restrict access to " +
             "the event provenance data (e.g. by strictly controlling the policies governing provenance for this Processor). " +
@@ -85,13 +85,29 @@ public final class AzureStorageUtils {
             .sensitive(true)
             .build();
 
+    public static final PropertyDescriptor STORAGE_SUFFIX = new PropertyDescriptor.Builder()
+            .name("storage-endpoint-suffix")
+            .displayName("Storage Endpoint Suffix")
+            .description(
+                    "Storage accounts in public Azure always use a common FQDN suffix " +
+                    "the preferred way is to configure them through a controller service specified in the Storage Credentials property. " +
+                    "The controller service can provide a common/shared configuration for multiple/all Azure processors. Furthermore, the credentials " +
+                    "can also be looked up dynamically with the 'Lookup' version of the service.")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .required(false)
+            .sensitive(false)
+            .build();
+
     public static final PropertyDescriptor CONTAINER = new PropertyDescriptor.Builder()
             .name("container-name")
             .displayName("Container Name")
-            .description("Name of the Azure storage container")
+            .description("Name of the Azure storage container (blob.core.windows.net) but in certain circumstances it will be necessary " +
+                    "to customize this endpoint with a different suffix. Examples of when this is needed: Private Links, Azure Stack, " +
+                    "non-public Azure regions")
             .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
             .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
-            .required(true)
+            .required(false)
             .build();
 
     public static final PropertyDescriptor PROP_SAS_TOKEN = new PropertyDescriptor.Builder()
@@ -131,7 +147,11 @@ public final class AzureStorageUtils {
      */
     public static CloudBlobClient createCloudBlobClient(ProcessContext context, ComponentLog logger, FlowFile flowFile) throws URISyntaxException {
         final AzureStorageCredentialsDetails storageCredentialsDetails = getStorageCredentialsDetails(context, flowFile);
-        final CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentialsDetails.getStorageCredentials(), true, null, storageCredentialsDetails.getStorageAccountName());
+        final CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(
+            storageCredentialsDetails.getStorageCredentials(),
+            true,
+            storageCredentialsDetails.getStorageSuffix(),
+            storageCredentialsDetails.getStorageAccountName());
         final CloudBlobClient cloudBlobClient = cloudStorageAccount.createCloudBlobClient();
 
         return cloudBlobClient;
@@ -151,6 +171,7 @@ public final class AzureStorageUtils {
 
     public static AzureStorageCredentialsDetails createStorageCredentialsDetails(PropertyContext context, Map<String, String> attributes) {
         final String accountName = context.getProperty(ACCOUNT_NAME).evaluateAttributeExpressions(attributes).getValue();
+        final String storageSuffix = context.getProperty(STORAGE_SUFFIX).evaluateAttributeExpressions(attributes).getValue();
         final String accountKey = context.getProperty(ACCOUNT_KEY).evaluateAttributeExpressions(attributes).getValue();
         final String sasToken = context.getProperty(PROP_SAS_TOKEN).evaluateAttributeExpressions(attributes).getValue();
 
@@ -168,7 +189,7 @@ public final class AzureStorageUtils {
             throw new IllegalArgumentException(String.format("Either '%s' or '%s' must be defined.", ACCOUNT_KEY.getDisplayName(), PROP_SAS_TOKEN.getDisplayName()));
         }
 
-        return new AzureStorageCredentialsDetails(accountName, storageCredentials);
+        return new AzureStorageCredentialsDetails(accountName, storageSuffix, storageCredentials);
     }
 
     public static Collection<ValidationResult> validateCredentialProperties(ValidationContext validationContext) {
